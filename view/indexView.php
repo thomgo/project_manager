e5bdd3cb3f5035552bebc2a8a4655fbe712bfa2c
Thomas Gossart <tg.59290@gmail.com>
2016-11-28 13:37:40 +0100

    first commit with a basic view structure based on boilerplate

---
+++ view/indexView.php
@@ -0,0 +1,15 @@
+<?php
+
+$pageTitle = "Une page de test";
+$pageDescription = "Vous voilà sur le première page du framework";
+
+include "header.php";
+
+?>
+
+<p>
+  Testons pour voir si cette relations marchent
+</p>
+
+
+<?php include "footer.php"; ?>
