3d008a3989b76f55912980040773aa1fb37b04c7
Thomas Gossart <tg.59290@gmail.com>
2017-01-22 23:00:15 +0100

    took of the rests from the view

--- view/indexView.php
+++ view/indexView.php
@@ -1,30 +1,11 @@
 <?php
 
-$pageTitle = "Une page de test";
-$pageDescription = "Vous voilà sur le première page du framework";
+$pageTitle = "";
+$pageDescription = "";
 
 include "header.php";
 
 ?>
-
-<p>
-  Testons cette relations marchent
-</p>
-
-<?php
-$form->formStart("index.php");
-$form->textInput("firstname", "nom du client", "required");
-$form->numberInput("age", "age du client");
-$form->maxFileSize("2000000");
-$form->fileInput("file/sample", "Votre fichier", "required", true);
-$form->submitButton("envoyer");
-$form->formEnd();
- ?>
-
-  <?php
-  // $fileManager->img($image);
-   ?>
-
 
 
 <?php include "footer.php"; ?>
